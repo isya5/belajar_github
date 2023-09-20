1644c020e51bf9709fc2c57ce20f5e3fdba47cab
daanloyo <145540217+isya5@users.noreply.github.com>
2023-09-20 15:07:01 +0700

    Create biodata.kt

---
+++ biodata.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+  var nama = "Agung Timotius Sinaga"
+  var kelas= "XI RPL 2"
+  var hobi = "Write Code"
+
+  println("Nama: $nama")
+  println("Kelas: $kelas")
+  println("Hobi: $hobi")
+}
